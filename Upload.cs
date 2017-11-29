73843348ff41e6568d8c9a35123d0cd08f8149bd
Reza <mrdev1020@gmail.com>
2017-11-29 22:02:49 +0330

    Update Upload.cs

--- Upload.cs
+++ Upload.cs
@@ -1,37 +1,62 @@
-﻿using System;
-using System.Collections.Generic;
+using MohamadReza;
+using System;
+using System.Drawing;
 using System.IO;
 using System.Linq;
-using System.Net;
 using System.Web;
-using System.Web.Mvc;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.Web.Helpers;
+using System.Threading.Tasks;
 
-namespace MohammadReza
+namespace Providers
 {
-    public class Upload : Controller 
+    public class Upload
     {
-        // 
-        private MohammadReza.Encryption enc = new Encryption();
-        // Image Valid Extension
+        private Encryption enc = new Encryption();
         private readonly string[] ImageExtensions = new string[] { ".jpeg", ".jpg", ".png", ".gif" };
-        //
         private string ImageName;
+        private string ImageName_thumb;
 
-        /**
-        *   Image Upload
-        *   
-        *   @param String DIR
-        *   @param HttpPostedFileBase File
-        *   @return String File Name
-        */
-        public string ImageUpload(string dir, HttpPostedFileBase file)
+
+        public string ImageUpload(string dir, int Width, int Height, HttpPostedFileBase file)
         {
-            if (file != null && file.ContentLength > 0 && ImageExtensions.Contains(System.IO.Path.GetExtension(file.FileName).ToLower()) && file.ContentType.Contains("image"))
+            if (file != null && file.ContentLength > 0 && ImageExtensions.Contains(Path.GetExtension(file.FileName).ToLower()) && file.ContentType.Contains("image"))
             {
                 try
                 {
-                    ImageName = enc.UUID() + System.IO.Path.GetExtension(file.FileName);
-                    string path = System.IO.Path.Combine(System.Web.Hosting.HostingEnvironment.MapPath(dir), System.IO.Path.GetFileName(ImageName));
+                    ImageName = enc.GUID() + Path.GetExtension(file.FileName);
+                    ImageName_thumb = enc.GUID() + "_thumb" + Path.GetExtension(file.FileName);
+
+                    string path = Path.Combine(System.Web.Hosting.HostingEnvironment.MapPath(dir), Path.GetFileName(ImageName));
+                    string path_tumb = Path.Combine(System.Web.Hosting.HostingEnvironment.MapPath(dir), Path.GetFileName(ImageName_thumb));
+
+                    file.SaveAs(path);
+                    Crop(Width, Height, file.InputStream, path_tumb);
+
+                    return ImageName;
+                }
+                catch (Exception)
+                {
+                    throw new Exception("Run again!");
+                }
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+
+
+        public string ImageUpload(string dir, HttpPostedFileBase file)
+        {
+            if (file != null && file.ContentLength > 0 && ImageExtensions.Contains(Path.GetExtension(file.FileName).ToLower()) && file.ContentType.Contains("image"))
+            {
+                try
+                {
+                    ImageName = enc.GUID() + Path.GetExtension(file.FileName);
+                    string path = Path.Combine(System.Web.Hosting.HostingEnvironment.MapPath(dir), Path.GetFileName(ImageName));
                     file.SaveAs(path);
                     return ImageName;
                 }
@@ -45,5 +70,15 @@ namespace MohammadReza
                 return null;
             }
         }
+
+
+
+        private static void Crop(int Width, int Height, Stream streamImg, string saveFilePath)
+        {
+            WebImage img = new WebImage(streamImg);
+            img.Resize(Width, Height);
+            img.Save(saveFilePath);
+        }
+
     }
 }
